90daf5254d2b82f7d2c80e29506e3d2d48409192
Aashutosh Dhar Dubey <ph-aashutoshdubey@mobiloitte.com>
2020-07-25 22:46:34 +0530

    adding header,adding footer fix ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header-footer/header/header.component';
+import { HeaderSecondComponent } from './header-footer/header-second/header-second.component';
+import { FooterComponent } from './header-footer/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeaderSecondComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Renderer2, Inject } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _renderer2: Renderer2,
+    @Inject(DOCUMENT) private document,
+    private elementRef: ElementRef
+  ) { }
 
   ngOnInit() {
+    this.jsText();
   }
+  jsText() {
+    const s = this.document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "assets/js/owl.carousel.min.js";
+    const __this = this; //to store the current instance to call
 
+    this.elementRef.nativeElement.appendChild(s);
+  }
 }
